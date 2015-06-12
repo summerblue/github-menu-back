2bc6348344325098f998cc30ffc3803b9f2e9870
Charlie Jade <summer.alex07@gmail.com>
2015-06-12 10:36:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+## `Github Menu Back` Chrome Extension
+
+I feel a lot of pain on the New Design of Github Top Menu, Can not go to my personal page by one click, so I decided to change it, so here comes this extention, for my beloving GitHub.
+
+## Download
+
+> [Google Chrome / Chromium](https://chrome.google.com/webstore/detail/mcobennfgmfddjmiikfamhhhiibcjbik)
+
+## Screenshots
+
+![](![screen-shot](https://cloud.githubusercontent.com/assets/324764/8122590/9be6d83c-10ee-11e5-949a-371bfd5ddbdd.png))
+
+## License
+
+Source code is release under [MIT license](http://mit-license.org/).
+Read LICENSE file for more information.
---
+++ js/script.js
@@ -0,0 +1,29 @@
+ $(document).ready(function () {
+
+    // For styling
+    $('body').attr('id', 'github-menu-back');
+
+    // If user is logged in
+    if ($('.header').hasClass('header-logged-in')) 
+    {
+        var avatar = $('#user-links li').last().find('img');
+
+        // 1. Replace the origin avatar
+        avatar.replaceWith('<span class="octicon octicon-octoface avatar" style="margin-top: 3px;"></span>');
+
+        // 2. Insert the old link
+        var avatar_link = avatar.attr('src');
+        var current_username = avatar.attr('alt').replace('@', '');
+        var new_link = '';
+        new_link += '<li class="header-nav-item dropdown js-menu-container">';
+        new_link += '    <a class="header-nav-link name" href="/'+current_username+'" data-ga-click="Header, go to profile, text:username">';
+        new_link += '      <img alt="@'+current_username+'" class="avatar" height="20" src="'+avatar_link+'" width="20">';
+        new_link += '      <span class="css-truncate">';
+        new_link += '        <span class="css-truncate-target">'+current_username+'</span>';
+        new_link += '      </span>';
+        new_link += '    </a>';
+        new_link += '</li>';
+        $('#user-links').prepend(new_link);
+    };
+
+});
